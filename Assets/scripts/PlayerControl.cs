73795f24abb938826f7d6169d5a4a2a04a32fcc7
bh2013 <bh2013@hw.ac.uk>
2025-01-30 11:42:25 +0000

    test

---
+++ Assets/scripts/PlayerControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    BoxCollider ninja;
+    public float speed;
+
+    float xInput;
+    float yInput;
+    bool jump;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        ninja = GetComponent<BoxCollider>();
+    }
+    void Update(){
+        if(transform.position.y < -5f){
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+        jump = Input.GetKeyDown(KeyCode.Space);
+
+        // rb.AddForce(xInput * speed, 0, yInput * speed);
+        rb.velocity = new Vector3(xInput * speed, rb.velocity.y, yInput * speed);
+        rb.AddForce(Vector3.down * 9.8f, ForceMode.Acceleration);
+        ninja.velocity = new Vector3(xInput * speed, rb.velocity.y, yInput * speed);
+    }
+}
